e4efa8eaf14e260f0ff023b90d1555d79fb6f47e
swas-998 <33576986+swas-998@users.noreply.github.com>
2020-04-18 14:04:52 +0530

    Init Commit

---
+++ public/js/app.js
@@ -0,0 +1,39 @@
+console.log("client side javascript file is loade")
+
+
+
+
+
+const weatherForm=document.querySelector('form')
+const search=document.querySelector('input')
+const msg1=document.querySelector("#message1")
+const msg2=document.querySelector('#message2')
+
+
+
+weatherForm.addEventListener('submit',(e)=>{
+
+    e.preventDefault()
+
+    const location=search.value
+    msg1.textContent="Loading ...."
+    msg2.textContent=""
+
+    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error)
+        {
+            msg1.textContent=data.error
+        }
+        else{
+       msg1.textContent=data.location
+        msg2.textContent=data.forecast
+        }
+
+        
+    })
+
+})
+
+    
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request =require("request")
+
+const forecast=(latitude,longitude,callback)=>{
+    const url = "https://api.darksky.net/forecast/c3d2dcaaeb2baace02c720fdeaf965ec/"+latitude+","+longitude+"?units=si&lang=en"
+    request({url,json:true},(error,{body})=>{
+        if(error)
+        {
+            callback('Unable to connect to weather service',undefined)
+        }else if(body.error)
+        {
+            callback("Unable to find the location coordinate Error",undefined)
+        }
+        else
+        {
+            callback(undefined, body.daily.data[0].summary+" It is currently " + body.currently.temperature + " degree celcius .There is " + body.currently.precipProbability + " % chance of rain ")
+        }
+
+    })
+    
+}
+module.exports=forecast
